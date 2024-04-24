10c9e13676c4d0466748b2c68a588cc034ae8013
Bexultan <bekaaaa121@gmail.com>
2024-04-25 00:08:25 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    public float airWalkSpeed = 3f;
+    public float jumpImpulse = 10f;
+    Vector2 moveInput;
+    TouchingDirections touchingDirections;
+
+    public float CurrentMoveSpeed {
+        get { 
+            if(isMoving && !touchingDirections.isOnWall) {
+
+                if(touchingDirections.isGrounded){
+                    if(isRunning) {
+                        return runSpeed;
+                    } else {
+                        return walkSpeed;
+                    }
+                } else {
+                    return airWalkSpeed;
+                }
+
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private bool _isMoving = false;
+
+    public bool isMoving { get
+        {
+            return _isMoving;
+        } 
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        } 
+    }
+
+    public bool _isRunning = false;
+
+    public bool isRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+
+    public bool isFacingRight { get {return _isFacingRight; } private set {
+        if(_isFacingRight != value){
+            transform.localScale *= new Vector2(-1, 1);
+        }
+
+        _isFacingRight = value;
+    } }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
+    }
+
+    private void FixedUpdate() 
+    {
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+
+        animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        isMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+        
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !isFacingRight) {
+            isFacingRight = true;
+        } else if(moveInput.x < 0 && isFacingRight) {
+            isFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context) {
+        if(context.started) {
+            isRunning = true;
+        } else if(context.canceled) {
+            isRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context) {
+        if(context.started && touchingDirections.isGrounded) {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+    }
+}
